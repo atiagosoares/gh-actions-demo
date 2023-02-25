25b56c60c3dfaf86d9f54ecfecb54f3b94f4e265
atiagosoares <atiagosoares@gmail.com>
2023-02-25 16:53:14 -0300

    Created simple flask hello world app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# Simple hello world app
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
